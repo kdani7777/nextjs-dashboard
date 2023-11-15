f6e7677bc0578569a5ee932c9ea16444214290df
Kush Dani <kushdani@Kushs-MacBook-Air.local>
2023-11-14 18:30:18 -0800

    remainder of course modules

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+import Dashboardskeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+    return <Dashboardskeleton />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-
 import Link from 'next/link';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
